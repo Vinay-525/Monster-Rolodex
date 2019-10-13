6977a8ddce023de3b9da20e90f4ccc550ffda627
Vinay-525 <vinaybansalvinay@gmail.com>
2019-10-13 21:01:28 +0530

    Updates

---
+++ precache-manifest.d0298a8eadded498c6ee0ba8f5c86435.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c708b2ecef823d1626476a06e8df25",
+    "url": "/Vinay-525/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "e132276e6e62e112b46f",
+    "url": "/Vinay-525/Monster-Rolodex/static/css/main.45c275b2.chunk.css"
+  },
+  {
+    "revision": "ad4462ccb75063d9b3da",
+    "url": "/Vinay-525/Monster-Rolodex/static/js/2.9d7d2cbd.chunk.js"
+  },
+  {
+    "revision": "e132276e6e62e112b46f",
+    "url": "/Vinay-525/Monster-Rolodex/static/js/main.1207173f.chunk.js"
+  },
+  {
+    "revision": "f70990917b69ab24c513",
+    "url": "/Vinay-525/Monster-Rolodex/static/js/runtime-main.de40c7d3.js"
+  }
+]);
